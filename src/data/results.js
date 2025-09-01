964ad68ae60dec5da9b697348aa3a74439b62acf
hanish476 <hanish476476@gmail.com>
2025-09-01 12:42:02 +0530

    program added

--- src/data/results.js
+++ src/data/results.js
@@ -174,10 +174,10 @@ export const results = [
       { grade: "A", studentId: "657" }, // 1 + 3 =
       { grade: "A", studentId: "662" }, // 1 + 3 =
       { grade: "A", studentId: "676" }, // 1 + 3 =
-     
+
     ]
   },
-    {
+  {
     programId: "A01",
     programName: "KHUṬBAH",
     category: "Aliya",
@@ -200,6 +200,149 @@ export const results = [
       { position: 1, grade: "-", studentId: "450" }, // 5 + 5 = 10
       { position: 2, grade: "-", studentId: "782" }, // 3 + 3 = 6
       { position: 3, grade: "-", studentId: "498" }, // 1 + 3 =  
+
+    ]
+  },
+  {
+    programId: "B01",
+    programName: "QIRĀʾAH",
+    category: "Bidaya",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "-", studentId: "806" }, // 5 + 5 = 10
+      { position: 2, grade: "-", studentId: "798" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "817" }, // 1 + 3 =  
+
+    ]
+  },
+  {
+    programId: "B10",
+    programName: "SPEECH ENG",
+    category: "Bidaya",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "-", studentId: "790" }, // 5 + 5 = 10
+      { position: 2, grade: "-", studentId: "799" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "816" }, // 1 + 3 =  
+
+    ]
+  },
+  {
+    programId: "B23",
+    programName: "HAND WRITING ARB",
+    category: "Bidaya",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "-", studentId: "809" }, // 5 + 5 = 10
+      { position: 2, grade: "-", studentId: "801" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "788" }, // 1 + 3 =  
+
+    ]
+  },
+  {
+    programId: "U01",
+    programName: "QIRĀʾAH",
+    category: "Ula",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "-", studentId: "753" }, // 5 + 5 = 10
+      { position: 2, grade: "-", studentId: "775" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "726" }, // 1 + 3 =  
+
+    ]
+  },
+   {
+    programId: "U02",
+    programName: "ḤIFẒ",
+    category: "Ula",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "-", studentId: "708" }, // 5 + 5 = 10
+      { position: 2, grade: "-", studentId: "724" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "753" }, // 1 + 3 =  
+
+    ]
+  },
+   {
+    programId: "U09",
+    programName: "CHAIN STORY",
+    category: "Ula",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "-", studentId: "722" }, // 5 + 5 = 10
+      { position: 2, grade: "-", studentId: "722" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "725" }, // 1 + 3 =  
+
+    ]
+  },
+   {
+    programId: "U11",
+    programName: "SPEECH ENG",
+    category: "Ula",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "A", studentId: "778" }, // 5 + 5 = 10
+      { position: 2, grade: "-", studentId: "748" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "710" }, // 1 + 3 =  
+
+    ]
+  },
+    {
+    programId: "U13",
+    programName: "WORD FIGHT ENG",
+    category: "Ula",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "B", studentId: "778" }, // 5 + 5 = 10
+      { position: 2, grade: "B", studentId: "748" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "710" }, // 1 + 3 =  
+
+    ]
+  },
+    {
+    programId: "TH01",
+    programName: "ḤIFẒ & QIRĀʾAH |",
+    category: "Thanawiyya",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "-", studentId: "607" }, // 5 + 5 = 10
+      { position: 2, grade: "-", studentId: "559" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "535" }, // 1 + 3 =  
+
+    ]
+  },
+   {
+    programId: "TH06",
+    programName: "SPEECH URD",
+    category: "Thanawiyya",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "-", studentId: "551" }, // 5 + 5 = 10
+      { position: 2, grade: "-", studentId: "585" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "543" }, // 1 + 3 =  
+
+    ]
+  },
+   {
+    programId: "TH17",
+    programName: "SPEECH URD",
+    category: "Thanawiyya",
+    programType: "single",
+    datePublished: "2025-09-1T16:00:00",
+    placements: [
+      { position: 1, grade: "-", studentId: "551" }, // 5 + 5 = 10
+      { position: 2, grade: "-", studentId: "585" }, // 3 + 3 = 6
+      { position: 3, grade: "-", studentId: "543" }, // 1 + 3 =  
 
     ]
   },
